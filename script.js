55844942296dcedc2abd38220235dd2540d96f80
Sérgio Júnior <sergiaraujo15@gmail.com>
2025-04-22 19:04:22 -0300

    feat: added parse references

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const infoDiv = document.getElementsByClassName("info")[0];
 const userInput = document.getElementById("user-input");
 const sendButton = document.getElementById("send-button");
 
+marked.use({
+  renderer: {
+    link({ href }) {
+      return href;
+    },
+  },
+});
+
 document.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    askQuestion();
+    sendButton.disabled == false && askQuestion();
   }
 });
 
@@ -15,6 +23,8 @@ sendButton.addEventListener("click", () => {
 });
 
 function askQuestion() {
+  sendButton.disabled = true;
+
   const userText = userInput.value;
 
   if (userText.trim() === "") {
@@ -31,16 +41,101 @@ function askQuestion() {
   // Resposta da API
   getBotResponse(userText).then((response) => {
     addMessage(response, "bot");
+
+    lucide.createIcons(); // Criar os novos ícones adicionados na mensagem
+
+    sendButton.disabled = false;
   });
 }
 
-function addMessage(text, sender) {
+function addMessage(message, sender) {
   const messageElement = document.createElement("div");
-
   messageElement.classList.add("message", sender);
-  messageElement.innerHTML = marked.parse(text);
+
+  if (sender == "bot") {
+    const referencesElement = document.createElement("ul");
+    referencesElement.className = "references-list";
+
+    const markdownTextParsedToHTML = marked.parse(message.content);
+
+    const parsedText = parseTextReferences(
+      {
+        ...message,
+        content: markdownTextParsedToHTML,
+      },
+      referencesElement
+    );
+
+    messageElement.innerHTML = parsedText;
+    messageElement.appendChild(referencesElement);
+  } else {
+    messageElement.innerHTML = message;
+  }
   chatBox.appendChild(messageElement);
   chatBox.scrollTop = chatBox.scrollHeight; // Role para a parte inferior
+}
+
+function parseTextReferences({ content, references }, referencesElement) {
+  const urlRegex = /{{\s*(https?:\/\/[^}]+)\s*}}/g;
+
+  const allReferencesUrls = Array.from(content.matchAll(urlRegex));
+
+  const prevUrls = [];
+
+  allReferencesUrls.forEach(([_, url], index) => {
+    const thisUrlAlreadyWasProceed = prevUrls.includes(url); // Evitar duplicar urls
+
+    const reference = references.find((re) => re.article.url === url);
+
+    if (!reference) return;
+
+    if (thisUrlAlreadyWasProceed == false) prevUrls.push(url);
+
+    const referenceElement = createReferenceLinkElement({
+      ...reference,
+      index: prevUrls.indexOf(url) + 1,
+    });
+
+    content = content.replace(`{{${url}}}`, referenceElement);
+
+    if (thisUrlAlreadyWasProceed == false)
+      referencesElement.append(createReferenceInfoElement(reference));
+  });
+
+  return content;
+}
+
+function createReferenceLinkElement(reference) {
+  const linkElement = document.createElement("a");
+  linkElement.href = `#r-${reference.article.url}`; // Quando clicar no <a>, vai para a referência do artigo embaixo da resposta
+  linkElement.className = "reference-link";
+  linkElement.textContent = reference.index;
+
+  return linkElement.outerHTML;
+}
+
+function createReferenceInfoElement(reference) {
+  const referenceElement = document.createElement("li");
+  const titleElement = document.createElement("h3");
+  const excerptElement = document.createElement("p");
+
+  const headerElement = document.createElement("div");
+  const urlElement = document.createElement("a");
+
+  headerElement.append(titleElement, urlElement);
+
+  referenceElement.id = "r-" + reference.article.url;
+
+  titleElement.innerText = reference.article.title;
+  excerptElement.innerText = reference.excerpt;
+
+  urlElement.href = reference.article.url;
+  urlElement.target = "_blank";
+  urlElement.innerHTML = `<i data-lucide="external-link"></i>`;
+
+  referenceElement.append(headerElement, excerptElement);
+
+  return referenceElement;
 }
 
 const url = "https://askscience-776890342339.southamerica-east1.run.app/question?q=";
@@ -51,8 +146,9 @@ async function getBotResponse(userInput) {
 
   console.log(data);
 
-  return data.content;
+  return data;
 }
-function reloadPage(){
+
+function reloadPage() {
   location.reload();
 }
